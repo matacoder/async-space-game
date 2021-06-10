21676af59c4bebbe7ca589fc89e2cb9ea19c599d
Denis Matakov <67960818+matacoder@users.noreply.github.com>
2021-06-10 08:28:11 +0000

    Stars are shining

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import asyncio
 import random
 
 TIC_TIMEOUT = 0.1
-
+STARS = '+*.:'
 
 def draw(canvas):
     row, column = curses.window.getmaxyx(canvas)
     canvas.border()
     curses.curs_set(False)
     coroutines = []
-    for i in range(50):
-        pos_x = random.randint(5, row - 5)
-        pos_y = random.randint(5, column - 5)
-        symbol = random.choice('+*.:')
+    for i in range(200):
+        pos_x = random.randint(2, row - 2)
+        pos_y = random.randint(2, column - 2)
+        symbol = random.choice(STARS)
         coroutine = blink(canvas, pos_x, pos_y, symbol)
         coroutines.append(coroutine)
 
@@ -22,31 +22,31 @@ def draw(canvas):
         for coroutine in coroutines.copy():
             try:
                 coroutine.send(None)
-                time.sleep(TIC_TIMEOUT)
+                # time.sleep(TIC_TIMEOUT)
+                canvas.refresh()
             except StopIteration:
                 coroutines.remove(coroutine)
         if len(coroutines) == 0:
             break
-        canvas.refresh()
-
+        
+async def sleep(tic=1):
+    for _ in range(tic*random.randint(30, 50)):
+        await asyncio.sleep(0)
 
 async def blink(canvas, row, column, symbol='*'):
     while True:
+        
         canvas.addstr(row, column, symbol, curses.A_DIM)
-        for _ in range(3):
-            await asyncio.sleep(0)
+        await sleep(20)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(1):
-            await asyncio.sleep(0)
+        await sleep(3)
 
         canvas.addstr(row, column, symbol, curses.A_BOLD)
-        for _ in range(2):
-            await asyncio.sleep(0)
+        await sleep(5)
 
         canvas.addstr(row, column, symbol)
-        for _ in range(1):
-            await asyncio.sleep(0)
+        await sleep(3)
 
 
 if __name__ == '__main__':
